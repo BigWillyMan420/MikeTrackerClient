e558c4ed77a440fb5e056c72551bdc89202aa122
mikequ <mikequdds@gmail.com>
2018-09-22 10:24:07 -0700

    Database
    Established database/location services

--- app/src/main/java/com/smarttracker/miketrackerclient/MainActivity.java
+++ app/src/main/java/com/smarttracker/miketrackerclient/MainActivity.java
@@ -13,6 +13,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Log.i("LOC", "permission ready");
+                    }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {/* ... */}
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {/* ... */}
+                }).check();
 
         Button registerButton = findViewById(R.id.button5);
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -30,45 +51,60 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+// Acquire a reference to the system Location Manager
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        Button enableDetection = findViewById(R.id.button3);
-        enableDetection.setOnClickListener(new View.OnClickListener() {
+        // Define a listener that responds to location updates
+        final LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                //makeUseOfNewLocation(location);
+                Log.i("LOC", location.getLatitude() + "" + location.getLongitude());
+
+                //a0001
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference inputLon = database.getReference("users/a0001/lon");
+                inputLon.setValue(location.getLongitude());
+                DatabaseReference inputLat = database.getReference("users/a0001/lat");
+                inputLat.setValue(location.getLatitude());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        Switch enableDetection = findViewById(R.id.button3);
+        enableDetection.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                // Acquire a reference to the system Location Manager
-                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-                // Define a listener that responds to location updates
-                LocationListener locationListener = new LocationListener() {
-                    public void onLocationChanged(Location location) {
-                        // Called when a new location is found by the network location provider.
-                        //makeUseOfNewLocation(location);
-                        Log.i("LOC", location.getLatitude() + "" + location.getLongitude());
-                    }
-
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-                    }
-
-                    public void onProviderEnabled(String provider) {
-                    }
-
-                    public void onProviderDisabled(String provider) {
-                    }
-                };
-
-                // Register the listener with the Location Manager to receive location updates
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    Log.i("LOC","started");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
                 }
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                else {
+                    locationManager.removeUpdates(locationListener);
+                }
             }
         });
+
     }
 }
--- app/src/main/java/com/smarttracker/miketrackerclient/RegisterActivity.java
+++ app/src/main/java/com/smarttracker/miketrackerclient/RegisterActivity.java
@@ -2,6 +2,12 @@ package com.smarttracker.miketrackerclient;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,6 +15,26 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText beaconEditText =  findViewById(R.id.editText);
+        final EditText nameEditText = findViewById(R.id.editText2);
+        final EditText passwordEditText = findViewById(R.id.editText3);
+
+        Button registerButton =  findViewById(R.id.button6);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference inputName = database.getReference("users/" + beaconEditText.getText().toString() + "/name");
+                DatabaseReference inputPassword = database.getReference("users/" + beaconEditText.getText().toString() + "/password");
+                DatabaseReference inputBeaconId = database.getReference("users/" + beaconEditText.getText().toString() + "/beaconId");
+
+                inputName.setValue(nameEditText.getText().toString());
+                inputPassword.setValue(passwordEditText.getText().toString());
+                inputBeaconId.setValue(beaconEditText.getText().toString());
+            }
+        });
     }
 }
 
